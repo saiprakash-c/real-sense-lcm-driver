9955dcf700057870e604a1b97a0670cc299c0eab
Sai Prakash Reddy C <saip@umich.edu>
2020-09-07 17:31:38 -0400

    moved the source files to src folder

--- src/ImageDisplay.cpp
+++ src/ImageDisplay.cpp
@@ -4,25 +4,34 @@ void ImageDisplay::handleColor(const lcm::ReceiveBuffer* buf, const std::string&
 {
     cv::Mat newImg(img->height,img->width, CV_8UC3, (void*)&(img->pixel_data[0]), img->line_stride);
     colorImg_ = newImg;
-    std::string time = std::to_string(img->utime);
-    std::string fName = "../images/" + time + ".png";
+    std::string timestamp = std::to_string(img->utime);
+    std::string fName = "../images/" + timestamp + ".png";
     cv::imwrite(fName, colorImg_ );
+    std::ofstream file;
+    file.open("../images/rgb.txt",std::ios::app);
+    if(!file.is_open()){std::cout << "rgb.txt could not be opened" << std::endl;}
+    file << time << "\n";
+    file.close();
     // Display in a GUI
     // cv::namedWindow("RGB Image", cv::WINDOW_AUTOSIZE);
-    cv::imshow("RGB Image", colorImg_);
-    cv::waitKey(1);
+    // cv::imshow("RGB Image", colorImg_);
+    // cv::waitKey(1);
 }
 
 void ImageDisplay::handleDepth(const lcm::ReceiveBuffer* buf, const std::string& channel, const DepthImage* img)
 {
     cv::Mat newImg(img->height,img->width, CV_16SC1, (void*)&(img->pixel_data[0]));
     depthImg_ = newImg;
-    std::string time = std::to_string(img->utime);
-    std::string fName = "../images/" + time + ".png";
+    std::cout<< img->meters_per_unit << "\n";
+    std::string timestamp = std::to_string(img->utime);
+    std::string fName = "../images/" + timestamp + ".png";
     cv::imwrite(fName, depthImg_ );
+    //  file.open("../images/depth.txt",std::ios::app);
+    // if(!file.is_open()){std::cout << ".txt could not be opened" << std::endl;}
+    // file << time << " " << fName << "\n";
     // Display in a GUI
-    cv::imshow("Depth Image", depthImg_);
-    cv::waitKey(1);
+    // cv::imshow("Depth Image", depthImg_);
+    // cv::waitKey(1);
 }
 
 void ImageDisplay::subscribe()
@@ -34,6 +43,6 @@ void ImageDisplay::subscribe()
 ImageDisplay::ImageDisplay(lcm::LCM& instance):
 lcmInstance_(instance)
 {
-    cv::namedWindow("RGB Image", cv::WINDOW_AUTOSIZE);
-    cv::namedWindow("Depth Image", cv::WINDOW_AUTOSIZE);
+    // cv::namedWindow("RGB Image", cv::WINDOW_AUTOSIZE);
+    // cv::namedWindow("Depth Image", cv::WINDOW_AUTOSIZE);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <librealsense2/rs.hpp> // Includes real sense cross platform API
-#include <lcm/lcm-cpp.hpp>
-#include "DepthImage.hpp"
-#include "Image.hpp"
+#include <lcm/lcm.h>
+#include "lcmtypes/DepthImage.h"
+#include "lcmtypes/Image.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    lcm::LCM lcm_object; // keep the correct address
+    lcm_t* lcm_object = lcm_create(NULL); // keep the correct address?
     rs2::pipeline p;     // create a pipe-line
-  
 
     //Create a configuration for configuring the pipeline with a non default profile
     rs2::config cfg;
 
     //Add desired streams to configuration
+    //TODO: option to add the configurations from the command-line
     cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 15);
     cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 15);
 
@@ -44,26 +44,25 @@ int main()
         uint8_t *depth_pixel_data = (uint8_t *)depth_frame.get_data();
 
         // publish color image
-        color_img.utime = color_frame.get_timestamp();
+        color_img.utime = color_frame.get_timestamp()*1e3;
         color_img.width = w_color;
         color_img.height = h_color;
         color_img.line_stride = color_frame.get_stride_in_bytes();
         color_img.bytes_per_pixel = color_frame.get_bytes_per_pixel();
         color_img.num_bytes = color_frame.get_data_size();
-        color_img.pixel_data.clear();
-        std::copy(color_pixel_data, color_pixel_data + (color_img.num_bytes), std::back_inserter(color_img.pixel_data));
-        lcm_object.publish("SENSOR_RGB_IMAGE", &color_img);
+        color_img.pixel_data = color_pixel_data;
+        Image_publish(lcm_object,"SENSOR_RGB_IMAGE", &color_img);
 
         // publish depth image
-        depth_img.utime = depth_frame.get_timestamp();
+        depth_img.utime = depth_frame.get_timestamp()*1e3; // get_timestamp outputs the time in milliseconds
         depth_img.width = w_depth;
         depth_img.height = h_depth;
         depth_img.line_stride = depth_frame.get_stride_in_bytes();
         depth_img.bytes_per_pixel = depth_frame.get_bytes_per_pixel();
         depth_img.num_bytes = depth_frame.get_data_size();
-        depth_img.pixel_data.clear();
-        std::copy(depth_pixel_data, depth_pixel_data + (depth_img.num_bytes), std::back_inserter(depth_img.pixel_data));
+        depth_img.pixel_data = depth_pixel_data;
         depth_img.meters_per_unit = depth_frame.get_units(); // conversion ratio from pixel value to distance in meters
-        lcm_object.publish("SENSOR_DEPTH_IMAGE", &depth_img);
+        DepthImage_publish(lcm_object,"SENSOR_DEPTH_IMAGE", &depth_img);
     }
+    return 0;
 }
